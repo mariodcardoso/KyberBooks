db70b5ee949109f88e580390486f56588f097d4f
Mario Dias <mario.dias@neon.com.br>
2021-10-16 19:14:15 -0300

    [new] Dashboard: Animating read progress values

--- app/src/main/java/br/com/kyberbooks/ui/dashboard/activities/DashboardActivity.kt
+++ app/src/main/java/br/com/kyberbooks/ui/dashboard/activities/DashboardActivity.kt
@@ -1,6 +1,11 @@
 package br.com.kyberbooks.ui.dashboard.activities
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.util.Log
+import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import br.com.kyberbooks.R
 import br.com.kyberbooks.databinding.ActivityDashboardBinding
@@ -9,6 +14,8 @@ import br.com.kyberbooks.ui.registerbook.activities.RegisterBookActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import dagger.hilt.android.AndroidEntryPoint
+import java.math.BigDecimal
+import java.math.BigInteger
 
 @AndroidEntryPoint
 class DashboardActivity : AppCompatActivity() {
@@ -18,6 +25,8 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupViews()
 
         with(binding) {
             btnRegisterBook.setOnClickListener {
@@ -32,6 +41,33 @@ class DashboardActivity : AppCompatActivity() {
             .load(R.drawable.img_profile_placeholder)
             .apply(RequestOptions.circleCropTransform())
             .into(binding.imgUser)
+    }
+
+    private fun setupViews() {
+        setupProgressBarAnimation()
+    }
+
+    private fun setupProgressBarAnimation() {
+
+        with(binding) {
+            ValueAnimator.ofInt(0, 375).apply {
+                addUpdateListener {
+                    val value = it.animatedValue as Int
+                    val progressValue = value * 100 / 500
+                    pgbReadProgress.progress = progressValue
+                    txtPercentReadPages.text = "$progressValue%"
+                    txtAbsolutReadPages.text = "$value / 500"
+                }
+
+                interpolator = LinearInterpolator()
+                duration = 2000
+                start()
+            }
+
+
+        }
+
+
     }
 
 }
