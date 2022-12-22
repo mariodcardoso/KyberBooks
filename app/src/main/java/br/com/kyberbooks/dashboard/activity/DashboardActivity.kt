4667db5cb1f1c55b592a453aa976ab5236de468f
elizandrareispereira <elizandrarpk@gmail.com>
2022-12-21 23:12:16 -0300

    iniciando construção do recycle view

--- app/src/main/java/br/com/kyberbooks/dashboard/activity/DashboardActivity.kt
+++ app/src/main/java/br/com/kyberbooks/dashboard/activity/DashboardActivity.kt
@@ -1,11 +1,9 @@
 package br.com.kyberbooks.dashboard.activity
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import br.com.kyberbooks.base.BaseActivity
-import br.com.kyberbooks.dashboard.viewmodel.DashboardViewModel
+import br.com.kyberbooks.dashboard.recycleview.DashboardAdapter
 import br.com.kyberbooks.databinding.ActivityDashboardBinding
-import com.bumptech.glide.Glide
 
 
 class DashboardActivity : BaseActivity() {
@@ -15,6 +13,9 @@ class DashboardActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.recycleDashboardListPublishingCompany
+        val listDashboardBookPublisher: DashboardAdapter
     }
 
 
---
+++ app/src/main/java/br/com/kyberbooks/dashboard/recycleview/DashboardAdapter.kt
@@ -0,0 +1,58 @@
+package br.com.kyberbooks.dashboard.recycleview
+
+import android.database.DataSetObserver
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListAdapter
+
+class DashboardAdapter: ListAdapter {
+
+
+    override fun registerDataSetObserver(observer: DataSetObserver?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun unregisterDataSetObserver(observer: DataSetObserver?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(position: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasStableIds(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getViewTypeCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun areAllItemsEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEnabled(position: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
