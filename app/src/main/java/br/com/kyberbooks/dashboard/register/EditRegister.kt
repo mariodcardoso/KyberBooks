cddc1c388bdc62ae614b79ae8ef8db22bcef64f0
elizandrareispereira <elizandrarpk@gmail.com>
2023-01-17 00:23:08 -0300

    criando evento de click para adicionar as edições

--- app/src/main/java/br/com/kyberbooks/dashboard/register/EditRegister.kt
+++ app/src/main/java/br/com/kyberbooks/dashboard/register/EditRegister.kt
@@ -1,6 +1,7 @@
 package br.com.kyberbooks.dashboard.register
 
 import android.os.Bundle
+import android.widget.Button
 import br.com.kyberbooks.dashboard.activity.DashboardActivity
 import br.com.kyberbooks.databinding.ActivityListBookEditBinding
 
@@ -15,8 +16,21 @@ open class EditRegister: DashboardActivity() {
         binding.textBookTitleEdit.setOnClickListener {
 
             val editTitle = binding.textBookTitleEdit.text.toString()
+            val editDescription = binding.textBookDescriptionEdit.text.toString()
+            val editIsbn = binding.textBookIsbnEdit.text.toString()
+            val editGenus = binding.textBookGenusEdit.text.toString()
 
-            binding.buttonBookSave.setText(editTitle)
+
+            binding.buttonBookSave.setText(editTitle, editDescription, editIsbn, editGenus)
         }
     }
+}
+
+private fun Button.setText(
+    editTitle: String,
+    editDescription: String,
+    editIsbn: String,
+    editGenus: String
+) {
+
 }
