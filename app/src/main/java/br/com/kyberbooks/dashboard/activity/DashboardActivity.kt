f224a1af1cdfd312857aaf67c8fd439f6e02b841
elizandrareispereira <elizandrarpk@gmail.com>
2023-01-31 19:33:59 -0300

    resolvendo conflitos

--- app/src/main/java/br/com/kyberbooks/dashboard/activity/DashboardActivity.kt
+++ app/src/main/java/br/com/kyberbooks/dashboard/activity/DashboardActivity.kt
@@ -1,9 +1,12 @@
 package br.com.kyberbooks.dashboard.activity
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import br.com.kyberbooks.base.BaseActivity
 import br.com.kyberbooks.dashboard.recycleview.DashboardAdapter
+import br.com.kyberbooks.dashboard.viewmodel.DashboardViewModel
 import br.com.kyberbooks.databinding.ActivityDashboardBinding
+import com.bumptech.glide.Glide
 
 
 open class DashboardActivity : BaseActivity() {
