a628cec93ddbb334edba64e9a91997a420247776
elizandrareispereira <elizandrarpk@gmail.com>
2023-03-22 07:18:48 -0300

    limpando código

--- app/src/main/java/br/com/kyberbooks/base/BaseViewModel.kt
+++ app/src/main/java/br/com/kyberbooks/base/BaseViewModel.kt
@@ -1,8 +1,6 @@
 package br.com.kyberbooks.base
 
 import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 
 open class BaseViewModel : ViewModel(), DefaultLifecycleObserver {
